e3fc5b0e6e3ce0c531b42cf5a43e8f8c8c419747
DebuggerAndrzej <redpandaworkstation@gmail.com>
2024-04-22 20:30:57 +0200

    Polish raw mode

--- vex.go
+++ vex.go
@@ -10,16 +10,33 @@ import (
 )
 
 func main() {
-	var err error
-	var char rune
 	previousState, err := term.MakeRaw(0)
+	if err != nil {
+		exitWithMessage("Failed to init raw terminal mode")
+	}
 	defer term.Restore(0, previousState)
+
+	enterReaderLoop()
+}
+
+func exitWithMessage(message string) {
+	fmt.Println(message)
+	os.Exit(1)
+}
+
+func enterReaderLoop() {
+	var char rune
+	var err error
 	reader := bufio.NewReader(os.Stdin)
-	for err == nil && string(char) != "q" {
+	for string(char) != "q" {
 		char, _, err = reader.ReadRune()
-		if unicode.IsControl(char) {
-			fmt.Printf("%v", char)
+		if err != nil {
+			exitWithMessage("Couldn't read inserted character")
 		}
-		fmt.Printf("%c", char)
+		if unicode.IsControl(char) {
+			fmt.Printf("%v\r\n", char)
+		} else {
+			fmt.Printf("%c\r\n", char)
+		}
 	}
 }
