14cdf2312ad229a5d28ea704eb272ad26b3ea9a6
DebuggerAndrzej <redpandaworkstation@gmail.com>
2024-04-25 20:20:06 +0200

    Fix for content showing, WSAD movement

--- editor/asciiConstants.go
+++ editor/asciiConstants.go
@@ -5,4 +5,7 @@ const ctrlKey = byte(0b00011111)
 const (
 	clearEntireScreen     = "\x1b[2J"
 	placeCursorAtBegining = "\x1b[H"
+	hideCursor            = "\x1b[?25l"
+	showCursor            = "\x1b[?25h"
+	eraseRestOfTheLine    = "\x1b[K"
 )
--- editor/editor.go
+++ editor/editor.go
@@ -4,13 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"golang.org/x/term"
 )
 
+type CursorPosition struct {
+	X, Y int
+}
+
 type Editor struct {
-	Rows    int
-	Columns int
+	Rows     int
+	Columns  int
+	Contents strings.Builder
+	Cursor   CursorPosition
 }
 
 func (editor *Editor) SetWindowSize() {
@@ -31,22 +38,49 @@ func (editor Editor) EnterReaderLoop() {
 			fmt.Print(clearEntireScreen)
 			fmt.Print(placeCursorAtBegining)
 			os.Exit(0)
+		case 'w', 's', 'a', 'd':
+			editor.moveCursor(char)
 		}
 	}
 }
 
-func (editor Editor) editorDrawRows() {
-	for i := 0; i < editor.Rows-1; i++ {
-		fmt.Print("~\r\n")
+func (editor *Editor) editorDrawRows() {
+	for y := 0; y < editor.Rows; y++ {
+		if y == editor.Rows/3 {
+			editorTitleMsg := "Vex editor - pre alpha"
+			pading := editor.Columns / 2
+			editor.Contents.WriteString(fmt.Sprintf("%*s", pading, editorTitleMsg))
+		} else {
+			editor.Contents.WriteString("~")
+		}
+		editor.Contents.WriteString(eraseRestOfTheLine)
+		if y < editor.Rows-1 {
+			editor.Contents.WriteString("\r\n")
+		}
 	}
-	fmt.Print("~")
 }
 
-func (editor Editor) refreshScreen() {
-	fmt.Print(clearEntireScreen)
-	fmt.Print(placeCursorAtBegining)
+func (editor *Editor) refreshScreen() {
+	editor.Contents.WriteString(hideCursor)
+	editor.Contents.WriteString(placeCursorAtBegining)
 	editor.editorDrawRows()
-	fmt.Print(placeCursorAtBegining)
+	editor.Contents.WriteString(fmt.Sprintf("\x1b[%d;%dH", editor.Cursor.Y, editor.Cursor.X))
+	editor.Contents.WriteString(showCursor)
+	fmt.Print(editor.Contents.String())
+	editor.Contents.Reset()
+}
+
+func (editor *Editor) moveCursor(char rune) {
+	switch char {
+	case 'w':
+		editor.Cursor.Y--
+	case 's':
+		editor.Cursor.Y++
+	case 'a':
+		editor.Cursor.X--
+	case 'd':
+		editor.Cursor.X++
+	}
 }
 
 func readKey(reader bufio.Reader) rune {
