86f4f51bdbb44ea083635906bd5b7785b59048d4
DebuggerAndrzej <redpandaworkstation@gmail.com>
2024-04-25 20:20:06 +0200

    Add movements
    Ahh sweet spaghetti code, should investigate possibility of moving some of the switches

--- editor/editor.go
+++ editor/editor.go
@@ -9,6 +9,18 @@ import (
 	"golang.org/x/term"
 )
 
+const (
+	ARROW_UP = 1000 + iota
+	ARROW_DOWN
+	ARROW_LEFT
+	ARROW_RIGHT
+	DEL_KEY
+	HOME_KEY
+	END_KEY
+	PAGE_UP
+	PAGE_DOWN
+)
+
 type CursorPosition struct {
 	X, Y int
 }
@@ -29,7 +41,7 @@ func (editor *Editor) SetWindowSize() {
 	editor.Columns, editor.Rows = columns, rows
 }
 
-func (editor Editor) EnterReaderLoop() {
+func (editor *Editor) EnterReaderLoop() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		editor.refreshScreen()
@@ -38,7 +50,19 @@ func (editor Editor) EnterReaderLoop() {
 			fmt.Print(clearEntireScreen)
 			fmt.Print(placeCursorAtBegining)
 			os.Exit(0)
-		case 'w', 's', 'a', 'd':
+		case PAGE_UP:
+			for i := 0; i < editor.Rows; i++ {
+				editor.moveCursor(ARROW_UP)
+			}
+		case PAGE_DOWN:
+			for i := 0; i < editor.Rows; i++ {
+				editor.moveCursor(ARROW_DOWN)
+			}
+		case HOME_KEY:
+			editor.Cursor.X = 0
+		case END_KEY:
+			editor.Cursor.X = editor.Columns - 1
+		case ARROW_UP, ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT:
 			editor.moveCursor(char)
 		}
 	}
@@ -72,14 +96,22 @@ func (editor *Editor) refreshScreen() {
 
 func (editor *Editor) moveCursor(char rune) {
 	switch char {
-	case 'w':
-		editor.Cursor.Y--
-	case 's':
-		editor.Cursor.Y++
-	case 'a':
-		editor.Cursor.X--
-	case 'd':
-		editor.Cursor.X++
+	case ARROW_UP:
+		if editor.Cursor.Y != 0 {
+			editor.Cursor.Y--
+		}
+	case ARROW_DOWN:
+		if editor.Cursor.Y != editor.Rows-1 {
+			editor.Cursor.Y++
+		}
+	case ARROW_RIGHT:
+		if editor.Cursor.X != editor.Columns-1 {
+			editor.Cursor.X++
+		}
+	case ARROW_LEFT:
+		if editor.Cursor.X != 0 {
+			editor.Cursor.X--
+		}
 	}
 }
 
@@ -88,5 +120,62 @@ func readKey(reader bufio.Reader) rune {
 	if err != nil {
 		ExitWithMessage("Couldn't read inserted character")
 	}
+
+	if doesStartWithEscapeCharacter(byte(char)) {
+		nextChar, _, _ := reader.ReadRune()
+		if nextChar == '[' {
+			nextNextChar, _, _ := reader.ReadRune()
+			if nextNextChar >= '0' && nextNextChar <= '9' {
+				nextNextNextChar, _, _ := reader.ReadRune()
+				if nextNextNextChar == '~' {
+					switch nextNextChar {
+					case '1', '7':
+						return HOME_KEY
+					case '3':
+						return DEL_KEY
+					case '4', '8':
+						return END_KEY
+					case '5':
+						return PAGE_UP
+					case '6':
+						return PAGE_DOWN
+					}
+				}
+			} else {
+
+				switch nextNextChar {
+				case 'A':
+					return ARROW_UP
+				case 'B':
+					return ARROW_DOWN
+				case 'C':
+					return ARROW_RIGHT
+				case 'D':
+					return ARROW_LEFT
+				case 'H':
+					return HOME_KEY
+				case 'F':
+					return END_KEY
+				}
+			}
+
+		}
+	}
+	if char == '0' {
+		nextChar, _, _ := reader.ReadRune()
+		switch nextChar {
+		case 'H':
+			return HOME_KEY
+		case 'F':
+			return END_KEY
+		}
+	}
 	return char
+}
+
+func doesStartWithEscapeCharacter(sequence byte) bool {
+	if sequence == 27 {
+		return true
+	}
+	return false
 }
