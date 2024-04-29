9ea3e3759d9240cbdb1a3969a8dfa11279e8381f
DebuggerAndrzej <redpandaworkstation@gmail.com>
2024-04-29 20:54:39 +0200

    A bit of recfactoring

---
+++ core/keys.go
@@ -0,0 +1,13 @@
+package core
+
+const (
+	ARROW_UP = 1000 + iota
+	ARROW_DOWN
+	ARROW_LEFT
+	ARROW_RIGHT
+	DEL_KEY
+	HOME_KEY
+	END_KEY
+	PAGE_UP
+	PAGE_DOWN
+)
--- vex.go
+++ vex.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 
-	"github.com/DebuggerAndrzej/vex/editor"
+	"github.com/DebuggerAndrzej/vex/core"
 
 	"golang.org/x/term"
 )
@@ -12,11 +12,11 @@ func main() {
 	flag.Parse()
 	previousState, err := term.MakeRaw(0)
 	if err != nil {
-		editor.ExitWithMessage("Failed to init raw terminal mode")
+		core.ExitWithMessage("Failed to init raw terminal mode")
 	}
 	defer term.Restore(0, previousState)
 
-	editor := editor.Editor{}
+	editor := core.Editor{}
 	editor.SetWindowSize()
 	editor.OpenFile(flag.Arg(0))
 	editor.SetStatusMessage("HELP: Ctrl-Q = quit")
